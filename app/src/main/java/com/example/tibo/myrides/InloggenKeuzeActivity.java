db4b650f6f71ed7f53c780add484987758d1b9c7
TiboRigole <tibo.rigole@gmail.com>
2018-10-10 00:15:50 +0200

    bezig in manifest file

---
+++ app/src/main/java/com/example/tibo/myrides/InloggenKeuzeActivity.java
@@ -0,0 +1,13 @@
+package com.example.tibo.myrides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InloggenKeuzeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inloggen_keuze);
+    }
+}
---
+++ app/src/main/java/com/example/tibo/myrides/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tibo.myrides;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //button voor naar de about page
+        Button naarAbout = (Button)findViewById(R.id.buttonAbout);
+
+        naarAbout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+
+
+
+        //button voor naar de inlog keuze page
+        Button naarKeuzeInloggen = (Button)findViewById(R.id.buttonInloggen);
+
+        naarKeuzeInloggen.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InloggenKeuzeActivity.class));
+            }
+        });
+    }
+
+
+}
