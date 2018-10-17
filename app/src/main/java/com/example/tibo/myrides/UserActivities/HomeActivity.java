3adc11ad13b2ee7637c2c9c75410761ec67cbf49
TiboRigole <tibo.rigole@gmail.com>
2018-10-17 22:52:41 +0200

    homepagina inlogbutton gemaakt, nog van facebook uitloggen handlen

--- app/src/main/java/com/example/tibo/myrides/UserActivities/HomeActivity.java
+++ app/src/main/java/com/example/tibo/myrides/UserActivities/HomeActivity.java
@@ -1,15 +1,62 @@
 package com.example.tibo.myrides.UserActivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.tibo.myrides.InlogActivity;
+import com.example.tibo.myrides.MainActivity;
 import com.example.tibo.myrides.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    //variable reservation
+
+    //buttons
+    private Button logOutButton;
+
+    //firebase authentication handler
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //firebase init
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        Log.d("FIREBASE", currentUser.toString());
+
+        //buttons init
+        logOutButton =(Button) findViewById(R.id.logOutButtonOnHomePage);
+
+        //logout button logica
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //log de user uit
+                logout(currentUser);
+
+                //@TODO: log uit van facebook ook
+
+                //ga terug naar de mainActivity
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+
+    //extra hulpmethodes
+    private void logout(FirebaseUser currentUser) {
+        mAuth.signOut();
     }
 }
