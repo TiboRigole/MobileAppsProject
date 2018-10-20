7cc8ecef5eb75335365e0c63469825414356b6c6
TiboRigole <tibo.rigole@gmail.com>
2018-10-20 14:25:37 +0200

    database setup OK.Firestore in Homeactivity.java
    daar zie je hoe alles verstuurd en gelezen kan worden
    zeer simpel. De database is 1 grote JSON
    read en write commandos ernaar toe
    
    als je een nieuwe table wilt maken hoeft je geen nieuwe table expliciet
    te maken. gewoon een ander woordje waar er 'users' staat,
     zorgt voor nieuwe table

--- app/src/main/java/com/example/tibo/myrides/UserActivities/HomeActivity.java
+++ app/src/main/java/com/example/tibo/myrides/UserActivities/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.tibo.myrides.UserActivities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.tibo.myrides.InlogActivity;
 import com.example.tibo.myrides.MainActivity;
 import com.example.tibo.myrides.R;
+import com.example.tibo.myrides.RegistreerActvity;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -25,6 +31,7 @@ public class HomeActivity extends AppCompatActivity {
     //buttons
     private Button logOutButton;
     private Button zendButton;
+    private Button naarSchoolVbButton;
 
     //editTexts
     private EditText aantalKmEditText;
@@ -35,7 +42,8 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
 
     //firebase database handler
-    private DatabaseReference mDatabase;
+    FirebaseFirestore db;
+    //https://firebase.google.com/docs/firestore/quickstart
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +56,14 @@ public class HomeActivity extends AppCompatActivity {
         Log.d("FIREBASE", currentUser.toString());
 
         //firebase database init
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+        //https://firebase.google.com/docs/firestore/quickstart
+
 
         //buttons init
         logOutButton =(Button) findViewById(R.id.logOutButtonOnHomePage);
         zendButton = (Button) findViewById(R.id.zendDBButton);
+        naarSchoolVbButton = (Button) findViewById(R.id.buttonschoolvoorbeeld);
 
         //edittexts init
         aantalKmEditText = (EditText) findViewById(R.id.editTextAantalKm);
@@ -87,8 +98,37 @@ public class HomeActivity extends AppCompatActivity {
                 Log.d("VALUES","betaald:"+betaald);
 
                 //verwerken en versturen?
-                mDatabase.child("/ritten/7VSSVrqriZcBIJVuc7fs").child("aantalKm").setValue(aantalKm);
-                mDatabase.child("/ritten/7VSSVrqriZcBIJVuc7fs").child("betaald").setValue(betaald);
+
+                //creeer een nieuwe User met een first en last name
+                Map<String, Object> user = new HashMap<>();
+                user.put("first","Ada");
+                user.put("last","Lovelace");
+                user.put("born",1815);
+
+                //voeg document toe met een gegenereerde ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("VALUES","DocumentSnapchat added with ID: "+ documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("VALUES", "Error adding document: "+ e);
+                            }
+                        });
+
+            }
+        });
+
+        //schoolvoorbeeld Button logica
+        naarSchoolVbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, SchoolVoorbeeldActivity.class));
             }
         });
 
---
+++ app/src/main/java/com/example/tibo/myrides/UserActivities/SchoolVoorbeeldActivity.java
@@ -0,0 +1,15 @@
+package com.example.tibo.myrides.UserActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tibo.myrides.R;
+
+public class SchoolVoorbeeldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_voorbeeld);
+    }
+}
